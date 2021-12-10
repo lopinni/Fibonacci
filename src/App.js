fa6ceade01ad9cb129d5e0b1e8f0c4d7f236a58f
Psze Nica <lopik98@gmail.com>
2021-12-10 19:13:02 +0100

    dodanie projektu

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(fibo(inputs.kword));
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <form onSubmit={handleSubmit}>
+          <p>
+            <label> Podaj liczbę porządkową (k):
+              <input type="number" name="kword"
+                value={inputs.kword || ""}
+                onChange={handleChange}
+              />
+            </label>
+          </p>
+          <input type="submit" value="Oblicz"/>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+function fibo(n) {
+  if (n==0) return 0;
+  if (n==1) return 1;
+  return fibo(n-1)+fibo(n-2);
+}
+
+export default App;
